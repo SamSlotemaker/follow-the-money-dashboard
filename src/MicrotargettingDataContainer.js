647b3c3a5b566e4f3c8051eb1e40788e6fdaaeef
Sam Slotemaker <sam.slotemaker@ziggo.nl>
2021-01-12 13:02:00 +0100

    toevoegen werkende radarchart

--- src/MicrotargettingDataContainer.js
+++ src/MicrotargettingDataContainer.js
@@ -1,44 +1,35 @@
 import React, { useEffect, useRef, useState } from 'react';
+import partijKleuren from './partijKleuren.js'
 import RadarChart from 'react-svg-radar-chart';
 import 'react-svg-radar-chart/build/css/index.css'
+import targetData from './fb_target.csv'
+const d3 = require('d3');
 // radar-chart van https://github.com/Spyna/react-svg-radar-chart
-
-const data = [
-    {
-        data: {
-            battery: 0.7,
-            design: .8,
-            useful: 0.9,
-            speed: 0.67,
-            weight: 0.8
-        },
-        meta: { color: '#3AB179' }
-    },
-    {
-        data: {
-            battery: 0.6,
-            design: .85,
-            useful: 0.5,
-            speed: 0.6,
-            weight: 0.7
-        },
-        meta: { color: 'red' }
-    }
-];
-
+console.log(partijKleuren)
 const captions = {
     // columns
-    battery: 'Battery Capacity',
-    design: 'Design',
-    useful: 'Usefulness',
-    speed: 'Speed',
-    weight: 'Weight'
+    '13-17': '13-17',
+    '18-24': '18-24',
+    '25-34': '25-34',
+    '35-44': '35-44',
+    '45-54': '45-54',
+    '55-64': '55-64',
+    '65+': '65+'
 };
+
+const partijen = [
+
+    'FvD',
+    'D66',
+    'VVD',
+    'PvdA'
+]
 
 
 export default function MicrotargettingDataContainer() {
 
     let container = useRef();
+    let [data, setData] = useState([])
     let [width, setWidth] = useState(0);
     let [height, setHeight] = useState(0)
     let [chartSize, setChartSize] = useState(0)
@@ -47,13 +38,37 @@ export default function MicrotargettingDataContainer() {
         setWidth(container.current.offsetWidth - container.current.offsetWidth / 10)
         setHeight(container.current.offsetHeight - container.current.offsetHeight / 10)
         setChartSize((container.current.offsetWidth < container.current.offsetHeight) ? container.current.offsetWidth - 50 : container.current.offsetHeight - 50)
-        console.log(container.current.offsetHeight, container.current.offsetWidth, chartSize)
+
+        d3.csv(targetData).then(result => {
+
+            let partijObjectenArray = [];
+            partijen.forEach(partij => {
+                partijObjectenArray.push(createDataObject(partij))
+            })
+            setData(partijObjectenArray)
+
+
+            function createDataObject(partij) {
+                const filteredResultMan = filterData(result, partij, 'man')
+                const filteredResultVrouw = filterData(result, partij, 'vrouw')
+                const countedResultMan = transformData(filteredResultMan)
+                const countedResultVrouw = transformData(filteredResultVrouw)
+                console.log(countedResultMan)
+                console.log(countedResultVrouw)
+                const combinedResults = combine(countedResultMan, countedResultVrouw)
+                const dataObject =
+                    { data: combinedResults, meta: { color: partijKleuren[partij] } }
+
+                return dataObject
+            }
+
+        })
     }, [])
 
     return (
         <section ref={container} className="micro-targetting-data-container">
             <h2>MicrotargettingDataContainer</h2>
-            {chartSize > 0 && <RadarChart
+            {data && <RadarChart
                 captions={captions}
                 data={data}
                 size={chartSize} />}
@@ -61,3 +76,40 @@ export default function MicrotargettingDataContainer() {
     )
 }
 
+
+function filterData(array, partij, column) {
+    return array.filter(item => {
+        if (item.partij == partij && item.gender == column) {
+            item['percentage geslacht/leeftijd'] = parseFloat(item['percentage geslacht/leeftijd'].replace(',', '.'))
+            return item
+        }
+    })
+}
+
+function transformData(array) {
+    let allAges = {
+        '13-17': 0,
+        '18-24': 0,
+        '25-34': 0,
+        '35-44': 0,
+        '45-54': 0,
+        '55-64': 0,
+        '65+': 0
+    }
+
+    array.forEach(item => {
+        if (allAges[item.age] == 0) {
+            allAges[item.age] += item['percentage geslacht/leeftijd']
+        }
+    })
+    return allAges
+}
+
+function combine(array1, array2) {
+    const newObject = {}
+    for (const key in array1) {
+        newObject[key] = (array1[key] + array2[key]) / 25
+    }
+    console.log(newObject)
+    return newObject
+}
---
+++ src/partijKleuren.js
@@ -0,0 +1,17 @@
+const partijKleuren = {
+  VVD: "#484d95",
+  PVV: "#00b8ff",
+  CDA: "#00914e",
+  D66: "#00b52f",
+  GroenLinks: "#00713b",
+  SP: "#db0372",
+  PvdA: "#aa0000",
+  ChristenUnie: "#0096b7",
+  PvdD: "#efc700",
+  "50Plus": "#cd7700",
+  SGP: "#7f8084",
+  DENK: "#00bec4",
+  FvD: "#7a0000",
+  PVDT: "#f8ec00"
+}
+export default partijKleuren
