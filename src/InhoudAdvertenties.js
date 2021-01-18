b22d9b96902f0444d9130437458d459218989177
Sam Slotemaker <sam.slotemaker@ziggo.nl>
2021-01-18 15:19:07 +0100

    toevoegen wordcloud

--- src/InhoudAdvertenties.js
+++ src/InhoudAdvertenties.js
@@ -1,11 +1,13 @@
 import React from 'react'
-
+import wordcloud from './images/wordcloud.png'
 export default function InhoudAdvertenties() {
     return (
         <section className="inhoud-advertenties-container">
 
             <h2>Inhoud advertenties</h2>
-
+            <div>
+                <img src={wordcloud} alt="" className="wordcloud" />
+            </div>
         </section>
     )
 }
